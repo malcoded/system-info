3b2c0d62ed74f46b02657f222a48eb0c653cf2dd
gildo.vargas@amazontic.pe <gildo.vargas@amazontic.pe>
2023-12-19 18:10:40 -0500

    Add Flask-CORS and update API route

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
 import platform
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-@app.route("/system_info", methods=["GET"])
+@app.route("/api/v1/system_info", methods=["GET"])
 def get_system_info():
     system_info = {
         "name": platform.node(),
