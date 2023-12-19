eb97ac4a2a56012c49044c08a2ad35099a934039
gildo.vargas@amazontic.pe <gildo.vargas@amazontic.pe>
2023-12-19 18:31:49 -0500

    Update get_system_info function to retrieve more system information

--- server.py
+++ server.py
@@ -8,10 +8,26 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route("/api/v1/system_info", methods=["GET"])
 def get_system_info():
-    system_info = {
-        "name": platform.node(),
-        "system": platform.system(),
-    }
+    # Obtenemos el nombre del dispositivo
+    device_name = platform.node()
+
+    # Obtenemos el tipo de procesador
+    processor = platform.processor()
+
+    # Obtenemos la cantidad de memoria RAM
+    ram = platform.ram()
+
+    # Obtenemos el ID del dispositivo
+    product_id = platform.product()
+
+    # Obtenemos el tipo de sistema operativo
+    system = platform.system()
+
+    # Obtenemos el ID del lápiz y la entrada táctil
+    pen_and_touch_input = platform._get_sys_info()["input"]["pen_and_touch_input"]
+
+    # Imprimimos la información obtenida
+    system_info = {device_name, processor, ram, product_id, system, pen_and_touch_input}
     return jsonify(system_info)
 
 
