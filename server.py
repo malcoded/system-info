1e8210258c70929275309a754d2e87f890dcadb8
gildo.vargas@amazontic.pe <gildo.vargas@amazontic.pe>
2023-12-21 14:45:08 -0500

    Refactor server.py: Remove unused code and update server configuration

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ from geopy.geocoders import Nominatim
 import socket
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 # Configura la aplicación Flask para trabajar detrás de un proxy
@@ -36,12 +35,6 @@ def obtener_ubicacion(latitud, longitud):
 def get_client_ip():
     # Intenta obtener la dirección IP desde el encabezado X-Forwarded-For
     # Si no está presente, utiliza request.remote_addr
-    return request.headers.get("X-Forwarded-For", request.remote_addr)
-
-
-def get_client_ip_f():
-    # Intenta obtener la dirección IP desde los encabezados X-Real-IP o X-Forwarded-For
-    # Si no están presentes, utiliza request.remote_addr
     return request.headers.get("X-Real-IP") or request.headers.get(
         "X-Forwarded-For", request.remote_addr
     )
@@ -68,27 +61,27 @@ def get_system_info():
         # Obtenemos el tipo de sistema operativo
         system = platform.system()
 
+        # Obtener el origen de la solicitud
+        origin = request.headers.get("Origin")
+
         # Obtenemos el ID del lápiz y la entrada táctil
         # pen_and_touch_input = platform._get_sys_info()["input"]["pen_and_touch_input"]
         ip_address = get_client_ip()
-        device_name_ip_f = get_client_ip_f()
         user_agent = request.user_agent.string
         location = obtener_ubicacion(latitud, longitud)
         mac_addresses = get_mac_address()
-        ipxd = request.remote_addr
-        device_name_ip = get_client_device_name(ipxd)
+        device_name_ip = get_client_device_name(ip_address)
         # Imprimimos la información obtenida
         system_info = {
             "device_name_server": device_name,
             "processor_server": processor,
             "system_server": system,
-            "ip_address": ip_address,
-            "device_name_ip_f": device_name_ip_f,
-            "user_agent": user_agent,
-            "location": location,
             "mac_addresses": mac_addresses,
             "device_name_ip": device_name_ip,
-            "ipxd": ipxd,
+            "origin": origin,
+            "ip_address": ip_address,
+            "user_agent": user_agent,
+            "location": location,
         }
         return jsonify(system_info)
     except Exception as e:
@@ -96,6 +89,4 @@ def get_system_info():
 
 
 if __name__ == "__main__":
-    from werkzeug.serving import run_simple
-
-    run_simple("0.0.0.0", 5000, app)
+    app.run(host="0.0.0.0", port=5000, debug=True)
